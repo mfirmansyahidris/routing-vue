a1691dcc12497765d6e954e91e7f2e40bec57002
manca_fi <manca.fi@gmail.com>
2019-09-18 18:05:11 +0800

    last init
    routing,
    viewing data,
    implementation store (action, getter, mutation, and setter),
    using SASS for styling

---
+++ src/data/dogs.js
@@ -0,0 +1,47 @@
+export default [
+    {
+        name: 'Sheeba',
+        breed: 'collie',
+        species: 'dog',
+        gender: 'female',
+        age: 7,
+        color: 'black/white',
+        weight: 34,
+        location: 'fourside',
+        notes: 'Pure breed. Trained for competition'
+    },
+    {
+        name: 'Hillary',
+        breed: 'mut',
+        species: 'dog',
+        gender: 'female',
+        age: 17,
+        color: 'orange/white',
+        weight: 37,
+        location: 'threed',
+        notes: 'Super friendly'
+    },
+    {
+        name: 'Zeus',
+        breed: 'afghan hound',
+        species: 'dog',
+        gender: 'female',
+        age: 7,
+        color: 'black/white',
+        weight: 34,
+        location: 'fourside',
+        notes: 'Pure breed. Trained for competition'
+    },
+    {
+        name: 'Kattie',
+        breed: 'golden retriever',
+        species: 'dog',
+        gender: 'female',
+        age: 2,
+        color: 'black',
+        weight: 44,
+        location: 'threed',
+        notes: 'Super friendly'
+    }
+
+]
---
+++ src/views/Dogs.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <h1>Dogs for Adoption</h1>
+        <b-table striped hover :items="dogs">
+            <template v-slot:cell(name)="data">
+                <router-link :to="`/pets/dogs/${data.index}`">
+                    {{data.value}}
+                </router-link>
+            </template>
+        </b-table>
+    </div>
+</template>
+
+<script>
+    import dogs from '@/data/dogs'
+
+    export default {
+        data() {
+            return {
+                dogs
+            }
+        }
+    }
+</script>
